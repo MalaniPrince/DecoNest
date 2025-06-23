6ddb542cecd327ffa4b4efcd460c4cb56548bc24
gondaliya <gondaliyachintan033@gmail.com>
2025-06-23 14:48:51 +0530

    sign up page

--- app/src/main/java/princeinfotech/deconest/ui/login/SignUpFragment.kt
+++ app/src/main/java/princeinfotech/deconest/ui/login/SignUpFragment.kt
@@ -24,7 +24,9 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.lo
+
+
+
     }
 
 }
