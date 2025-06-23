25c48fe2d9748c06b32cf4c3917920c721e1ef97
Prince(Android) <malaniprince9@gmail.com>
2025-06-23 14:23:45 +0530

    SignUp Screen

---
+++ app/src/main/java/princeinfotech/deconest/ui/login/SignUpFragment.kt
@@ -0,0 +1,30 @@
+package princeinfotech.deconest.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import princeinfotech.deconest.R
+import princeinfotech.deconest.databinding.FragmentLoginBinding
+import princeinfotech.deconest.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lo
+    }
+
+}
