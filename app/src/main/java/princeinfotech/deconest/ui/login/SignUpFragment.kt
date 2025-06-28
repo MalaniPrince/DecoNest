b0185ddbc36f0e9fe799ec66a329dc0b42ff333f
Prince(Android) <malaniprince9@gmail.com>
2025-06-28 10:06:59 +0530

    Firebase Works Pending

--- app/src/main/java/princeinfotech/deconest/ui/login/SignUpFragment.kt
+++ app/src/main/java/princeinfotech/deconest/ui/login/SignUpFragment.kt
@@ -1,17 +1,24 @@
 package princeinfotech.deconest.ui.login
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import princeinfotech.deconest.R
 import princeinfotech.deconest.databinding.FragmentLoginBinding
 import princeinfotech.deconest.databinding.FragmentSignUpBinding
+import princeinfotech.deconest.ui.home.HomeMainActivity
 
 class SignUpFragment : Fragment() {
     private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +32,17 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       binding.SignupButton.setOnClickListener {
-            findNavController().navigate(R.id.LoginFragment)
+        binding.SignupButton.setOnClickListener {
+            val email = binding.EnterEmail.text.toString()
+            val password = binding.EnterComPassword.text.toString()
+
+            if(email =="admin" && password=="123"){
+                startActivity(Intent(requireContext(),HomeMainActivity::class.java))
+            }
+
         }
-        binding.buttonLogin.setOnClickListener {
-            findNavController().navigate(R.id.LoginFragment)
+            binding.buttonLogin.setOnClickListener {
+                findNavController().navigate(R.id.LoginFragment)
+            }
         }
     }
-
-}
