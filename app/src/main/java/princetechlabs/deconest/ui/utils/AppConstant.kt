fbe367f440e1686830eb3aac1f4509691f868baf
Priyanshu <priyanshuparjaparti@gmail.com>
2026-01-06 17:50:41 +0530

    extra

--- app/src/main/java/princetechlabs/deconest/ui/utils/AppConstant.kt
+++
@@ -1,7 +0,0 @@
-package princetechlabs.deconest.ui.utils
-
-class AppConstant {
-    companion object{
-        const val WEB_CLIENT_ID = "http://817643624565-draj9f243r22reao8qcm8mhrra0eliol.apps.googleusercontent.com"
-    }
-}
