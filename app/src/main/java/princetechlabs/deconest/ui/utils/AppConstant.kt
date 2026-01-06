aed0b26ca0821cf8bee80a0d38155a4c327e6ceb
Prince(Android) <malaniprince9@gmail.com>
2026-01-06 17:46:49 +0530

    Solving Google Authetication Error

--- app/src/main/java/princetechlabs/deconest/ui/utils/AppConstant.kt
+++ app/src/main/java/princetechlabs/deconest/ui/utils/AppConstant.kt
@@ -2,6 +2,6 @@ package princetechlabs.deconest.ui.utils
 
 class AppConstant {
     companion object{
-        const val WEB_CLIENT_ID = "http://817643624565-draj9f243r22reao8qcm8mhrra0eliol.apps.googleusercontent.com"
+        const val WEB_CLIENT_ID = "817643624565-draj9f243r22reao8qcm8mhrra0eliol.apps.googleusercontent.com"
     }
 }
