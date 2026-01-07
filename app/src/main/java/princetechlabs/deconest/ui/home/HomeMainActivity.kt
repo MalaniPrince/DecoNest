c1548f0db565c3bf6bf14e12c7c590ec9a5b3d75
Priyanshu <priyanshuparjaparti@gmail.com>
2026-01-07 13:43:37 +0530

    Priyanshu dev

--- app/src/main/java/princetechlabs/deconest/ui/home/HomeMainActivity.kt
+++ app/src/main/java/princetechlabs/deconest/ui/home/HomeMainActivity.kt
@@ -1,14 +1,20 @@
 package princetechlabs.deconest.ui.home
+
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import princetechlabs.deconest.R
 import princetechlabs.deconest.databinding.ActivityMainBinding
+import princetechlabs.deconest.ui.BottomNavigation.AccountFragment
+import princetechlabs.deconest.ui.BottomNavigation.CartFragment
+import princetechlabs.deconest.ui.BottomNavigation.CategoryFragment
 
 class HomeMainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +23,12 @@ class HomeMainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
 
+        val navController = navHostFragment.navController
 
-
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
-
 }
